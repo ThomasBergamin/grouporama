954505f831942800487cc27d059664563d7268b6
Thomas <thomas.bergamin@placemeet.fr>
2021-10-15 11:34:01 +0200

    style: 💄 init use of tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/react-in-jsx-scope */
+import logo from './img/logo.svg';
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+  <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+    <div className="flex-shrink-0">
+      <img className="h-12 w-12" src={logo} alt="ChitChat Logo" />
+    </div>
+    <div>
+      <div className="text-xl font-medium text-primary">ChitChat</div>
+      <p className="text-gray-500">You have a new message!</p>
+    </div>
   </div>
 );
 
